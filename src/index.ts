e6e5623b744c91c88bf09f3e53f079d514bd70fe
refrain62 <nao_h13@hotmail.com>
2023-06-20 01:14:52 +0900

    型定義があいまいだったので追加

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,45 @@
 import express from "express"; // ES2015形式で記載する
 const app = express();
-const port = 3000;
+const port: number = 3000;
 
-// 簡易的にデータを定義
-const members = [{
-    id: "1",
+// 型定義
+type Member = {
+    id: number
+    name: string
+    team: string
+};
+
+// データを定義
+const members: Member[] = [{
+    id: 1,
     name: "Taro",
     team: "A"
   }, {
-    id: "2",
+    id: 2,
     name: "Jiro",
     team: "B"
   }, {
-    id: "3",
+    id: 3,
     name: "Saburo",
     team: "A"
   }];
 
 console.log("hello");
 
-app.get('/', (req, res) => {
+app.get('/', (req: express.Request, res: express.Response) => {
     res.status(200).send("Hello World");
 });
 
 // パスパラメータを設定しない場合は、全メンバーの情報取得 localhost:3000/members
-app.get('/members', (req, res) => {
-    res.status(200).send(members);
+app.get('/members', (req: express.Request, res: express.Response) => {
+    res.status(200).send(JSON.stringify(members));
 });
 
 // パスパラメータを設定する場合 localhost:3000/members/2
-app.get('/members/:id', (req, res) => {
-    const id = req.params.id;
-    const member = members.filter((member) => member.id === id);
-    res.status(200).send(member);
+app.get('/members/:id', (req: express.Request, res: express.Response) => {
+    const id: string = req.params.id;
+    const member: Member[] = members.filter((member) => member.id === Number(id));
+    res.status(200).send(JSON.stringify(member));
 });
 
 app.listen(port, () => console.log(`Example app membersening on port ${port}!`));
